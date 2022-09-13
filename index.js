7bac7df4f9d15c68c4141b54e39c92a6079fef5e
Ambar <Ambarpg80@gmail.com>
2022-09-13 14:33:07 -0400

    commit tree 0681f3e14e21ebbbde22aaf12500e0e2206c0a13

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+  
+function findMatching(array, string){
+    
+    const result = array.filter(item =>item.toUpperCase() === string.toUpperCase())
+        //
+        return result   
+    }
+
+findMatching();
+
+function fuzzyMatch(array, string){
+const result = array.filter(item => item.startsWith(string))
+    //console.log(result)
+    return result
+}
+
+fuzzyMatch();
+
+function matchName(array, string){
+const result = array.filter(item => item.name === string)
+
+        return result
+}
+
+matchName();
